c90b84edc9b9a03c3ff881195a8bf642bc3d7451
JuditaVa <judita_f@yahoo.com>
2019-05-06 21:35:47 +0300

    namu darbai PHP 01 02 03

---
+++ namudarbai/PHP/index01ir02.php
@@ -0,0 +1,10 @@
+<ul>
+    <li><?php echo $country = 'Lithuania'; ?></li>
+    <li><?php echo $city = 'Vilnius'; ?></li>
+    <li><?php echo $population = 574221; ?></li>
+</ul>
+
+<ul>
+<li><?php echo $city .'- Lietuvos sostinė'; ?></li>
+<li><?php echo "$city ir $country sparciai auga"; ?></li>
+</ul>
---
+++ namudarbai/PHP/index03.php
@@ -0,0 +1,19 @@
+<?php
+    $x = 10;
+    $y = 7;
+?>
+<ul>
+    <?php  $z=$x + $y; ?>
+<li><?php echo "$x + $y = $z" ; ?></li>
+    <?php  $z=$x - $y; ?>
+<li><?php echo "$x - $y = $z" ; ?></li>
+    <?php  $z=$x * $y; ?>
+<li><?php echo "$x * $y = $z"; ?></li>
+    <?php  $z=$x / $y; ?>
+<li><?php echo "$x / $y = $z"; ?></li>
+    <?php  $z=$x % $y; ?>
+<li><?php echo "$x % $y = $z"; ?></li>
+</ul>
+
+
+
